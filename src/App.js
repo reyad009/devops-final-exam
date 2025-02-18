26f8545eaad1ec468a9e623111c2e0cb124e8bfe
Reyad Hasan <reyad.hasan@pm.me>
2025-02-18 21:11:09 +0600

    Initial commit for reyad-react project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, My Name is Reyad</h1>
+        <p>Welcome to my React App!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
